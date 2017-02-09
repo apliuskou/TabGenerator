1400a2a0d3a1991b3eaec2b3673288ea68a12929
fincalc <777alexbel777@gmail.com>
2017-02-09 05:59:21 +0300

    Table generated is ready

---
+++ GenerateRandomDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabGenerator
+{
+    class GenerateRandomDate
+    {
+
+        public static string SalesDate()
+        {
+            Random randomDay = new Random();
+            int day = randomDay.Next(1, 30); //Тупо генерируем целое число от 1 до 30
+
+            Random randomMonth = new Random();
+            int month = randomMonth.Next(1, 12); //Тупо генерируем целое число от 1 до 12
+
+            Random randomYear = new Random();
+            int year = randomYear.Next(1995, 2016); //Тупо генерируем целое число от 1995 до 2016
+
+            string _day;
+            _day = Convert.ToString(day); //Конвертим день в стринг
+
+            string _month;
+            _month = Convert.ToString(month); //Конвертим номер месяца в стринг
+
+            string _year;
+            _year = Convert.ToString(year); //Конвертим год в стринг
+
+            string dayOfTransaction = _year + ", " + _month + ", " + _day; //Формируем строку "Дата транзакции"
+            //У тебя эта дата записана иначе, в формате времени: гггг/мм/дд  чч:мм:сс
+            //Но я пока оставлю запятые. Вдруг это будет критично при анализе файла.
+
+
+            return dayOfTransaction;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//Без этой строки StreamWriter не работал и не распознавался
+
+
+
+
+
+namespace TabGenerator
+{
+    class Program
+    {
+        //? А почему у тебя генерация случайной даты шла до void Main? Так можно? Или так надо в каком-то конкретном случае?
+        //Я где-то прочёл, что программа начнёт исполняться с этого места, с void Main
+
+        static void Main(string[] args)
+
+        {
+        //Генерируем случайную дату в отдельном классе, просто целые числа, пока не в формате времени
+           string dateOfTransaction = GenerateRandomDate.SalesDate();
+
+            string homeFolder = @"c:\GitHub\tabgen\";
+            Random moneyRandom = new Random();
+            Random randomPartner = new Random();
+            string nextRecord = "";
+            string[] allPossibleBanks = Enum.GetNames(typeof(Partners));
+            int numberOfPartners = allPossibleBanks.Length;
+
+
+
+            try
+            {
+                using (StreamWriter target = File.CreateText(homeFolder + @"\tableofbanks2.csv"))
+                {
+
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        nextRecord = nextRecord + i + ", " + (Partners)randomPartner.Next(0, numberOfPartners) + ", ";
+                        nextRecord = nextRecord + moneyRandom.Next(999, 100000) + ", ";
+                        nextRecord = nextRecord + dateOfTransaction;
+                        Console.WriteLine(" Line{0}: {1}", i, nextRecord);
+                        target.WriteLine(nextRecord);
+
+                        nextRecord = "";
+                    }
+                }
+            }
+            catch (IOException)
+            {
+
+                Console.WriteLine("Error! File already exist or path not found!");
+                throw;
+            }
+
+            Console.WriteLine("Saved successfully! Table of banks built!");
+            Console.ReadKey();
+
+
+
+
+        }
+
+        
+    }
+}
